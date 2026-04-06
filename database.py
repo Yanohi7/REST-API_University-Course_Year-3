a1d0d5a094c55e5be7b2ca1ba935827a94478b0b
Andriy <andrii.tsikhovskyi.22@pnu.edu.ua>
2026-04-07 01:38:38 +0300

    lab 2 fixes

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "postgresql+psycopg2://postgres:123456@localhost:5555/library_db"
 )
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
